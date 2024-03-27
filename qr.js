c64c689109da273754b5306aadc0f66415fd5d6a
Lucio Cienfuegos <luciocienfuegos@gmail.com>
2024-03-26 22:39:01 -0300

    reload

--- qr.js
+++ qr.js
@@ -9,34 +9,37 @@ class QR {
 }
 
 let QRS = JSON.parse(localStorage.getItem('QRS')) || [];
-let dias = ['martes', 'miercoles', 'jueves' , 'viernes'];
+let dias = ['martes', 'miercoles', 'jueves', 'viernes'];
 
 function generarQR() {
     const producto = document.getElementById('producto').value;
     const descuento = document.getElementById('porcentaje').value;
     const dia = document.getElementById('dia').value;
-    
+
     // Verificar que todos los campos tengan un valor
     if (producto && descuento && dia) {
         const link = generarLink(dia);
-        
+
         const nuevoQR = new QR(producto, descuento, dia, link, null); // Inicializar la propiedad codigoURL con null
-        
+
         const codigo = new QRCode(contenedorQR);
         codigo.makeCode(link);
-        
+
         // Obtener la URL de la imagen del código QR después de un breve retraso
         setTimeout(() => {
             const codigoURL = contenedorQR.querySelector('img').src;
             nuevoQR.codigoURL = codigoURL; // Asignar la URL de la imagen del código QR a la propiedad codigoURL
-            
+
             QRS.push(nuevoQR); // Guardar el nuevo QR en el arreglo QRS
             localStorage.setItem('QRS', JSON.stringify(QRS)); // Guardar el arreglo QRS en el localStorage
             crearDescuento(dia); // Mostrar el cartel para el nuevo QR
+            location.reload();
         }, 500); // Esperar 500 milisegundos (ajusta este valor según sea necesario)
     } else {
         alert('Por favor, completa todos los campos antes de generar el código QR.');
     }
+    
+
 }
 
 function generarLink(dia) {
@@ -46,11 +49,11 @@ function generarLink(dia) {
 function crearDescuento(dia) {
     const cartel = document.getElementById(`${dia}`);
     cartel.classList.add('cartel');
-    
-    
+
+
     // Invertimos el arreglo QRS para buscar del más nuevo al más viejo
     const qrEncontrado = QRS.slice().reverse().find(qr => qr.dia === dia);
-    
+
     if (qrEncontrado) {
         const contenidoCartel = `
             <p>Producto: ${qrEncontrado.producto}</p>
@@ -59,10 +62,10 @@ function crearDescuento(dia) {
             <img id="" src="${qrEncontrado.codigoURL}" alt="">
         `;
         cartel.innerHTML = contenidoCartel;
-    } 
+    }
 }
 
-function mostrarCarteles(){
+function mostrarCarteles() {
     for (let i = QRS.length - 1; i >= 0; i--) {
         const qr = QRS[i];
         crearDescuento(qr.dia);
@@ -71,6 +74,7 @@ function mostrarCarteles(){
 
 function borrarItemLocalStorage(clave) {
     localStorage.removeItem(clave);
+    location.reload();
 }
 
 mostrarCarteles()
